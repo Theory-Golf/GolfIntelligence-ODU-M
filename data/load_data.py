df7bab00dc9e37b5ed6262a6fa84fd2eaca3ad06
SGCanalytics <47985007+SGCanalytics@users.noreply.github.com>
2026-03-03 07:32:50 -0500

    Update load_data.py

--- data/load_data.py
+++ data/load_data.py
@@ -5,7 +5,7 @@ import streamlit as st
 # CONFIG
 # ============================================================
 
-SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZZ8-dHrvrfl8YQnRSLpCYS6GjTHpXQm2uVuqS0X5t3yOxhciFnvxlLSSMX_gplveVmlP5Uz8nOmJF/pub?gid=0&single=true&output=csv"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdJBXisvzgd4e4llD2ILaaqMCUMrN44e27wgmGSM2HagqDk-JNUfnHt7siGA_pNNXR5nGDLuimFype/pub?output=csv"
 
 
 # ============================================================
